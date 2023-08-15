1b7ecb40607d92d82bd977da945a1b7a9d770976
Percy Maldonado <maldonadoq.percy@gmail.com>
2023-08-15 14:30:10 -0300

    initial commit
    create project

---
+++ Model/ContactContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phonebook.Model
+{
+    public class ContactContext: DbContext
+    {
+        public DbSet<Contact> Contacts { get; set;}
+
+        public string path = @"E:\University\Microsoft\Phonebook\Phonebook.db";
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder.UseSqlite($"Data Source={path}");
+    }
+}
