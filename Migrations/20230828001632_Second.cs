f3f374b4c917849274cf542a22c6f706440b8250
Percy Maldonado <maldonadoq.percy@gmail.com>
2023-08-28 02:53:11 -0300

    model migrations

---
+++ Migrations/20230828001632_Second.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Phonebook.Migrations
+{
+    /// <inheritdoc />
+    public partial class Second : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "category",
+                table: "Contacts",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "category",
+                table: "Contacts");
+        }
+    }
+}
