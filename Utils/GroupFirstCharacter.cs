e1623b63bcd291a4f04f3ee784237aa8a9276b31
Percy Maldonado <maldonadoq.percy@gmail.com>
2023-08-28 02:53:45 -0300

    group comparation class

---
+++ Utils/GroupFirstCharacter.cs
@@ -0,0 +1,25 @@
+﻿using Phonebook.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phonebook.Utils
+{
+    internal class GroupFirstCharacter : GroupDescription
+    {
+        public override object GroupNameFromItem(object item, int level, CultureInfo culture)
+        {
+            Contact? v = item as Contact;
+
+            if (v != null)
+            {
+                return v.name[0];
+            }
+            return item;
+        }
+    }
+}
